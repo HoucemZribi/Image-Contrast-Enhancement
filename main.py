cf004ce95c97b1250826c2ca5ee3a9d4264f5ffe
HoucemZribi <houcem.zribi@isimg.tn>
2022-01-12 21:50:17 +0100

    Fuzzy task

--- main.py
+++ main.py
@@ -35,5 +35,35 @@ def VeryBright(x, M):
 def ExtremelyBright(x, M):
     return G(x, 305, (255-M)/6)
 
+for M in (128, 64, 192):
+    x = np.arange(-50, 306)
 
+    ED = ExtremelyDark(x, M)
+    VD = VeryDark(x, M)
+    Da = Dark(x, M)
+    SD = SlightlyDark(x, M)
+    SB = SlightlyBright(x, M)
+    Br = Bright(x, M)
+    VB = VeryBright(x, M)
+    EB = ExtremelyBright(x, M)
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(x, ED, 'k-.', label='ED', linewidth=1)
+    plt.plot(x, VD, 'k-', label='VD', linewidth=2)
+    plt.plot(x, Da, 'g-', label='Da', linewidth=2)
+    plt.plot(x, SD, 'b-', label='SD', linewidth=2)
+    plt.plot(x, SB, 'r-', label='SB', linewidth=2)
+    plt.plot(x, Br, 'c-', label='Br', linewidth=2)
+    plt.plot(x, VB, 'y-', label='VB', linewidth=2)
+    plt.plot(x, EB, 'y-.', label='EB', linewidth=1)
+    plt.plot((M, M), (0, 1), 'm--', label='M', linewidth=2)
+    plt.plot((0, 0), (0, 1), 'k--', label='MinIntensity', linewidth=2)
+    plt.plot((255, 255), (0, 1), 'k--', label='MaxIntensity', linewidth=2)
+    plt.legend()
+    plt.xlim(-50, 305)
+    plt.ylim(0.0, 1.01)
+    plt.xlabel('Pixel intensity')
+    plt.ylabel('Degree of membership')
+    plt.title(f'M={M}')
+    #plt.show()
 
