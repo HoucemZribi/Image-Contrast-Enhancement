6a88421231a58fc345707df6c3c5212c53dd30ff
HoucemZribi <houcem.zribi@isimg.tn>
2022-01-14 21:08:39 +0100

    Last commit

--- main.py
+++ main.py
@@ -120,3 +120,75 @@ for pixel in (64, 96, 160, 192):
     plt.ylabel('Degree of membership')
     plt.title(f'input_pixel_intensity = {pixel}\nM = {M}')
    # plt.show()
+   def FuzzyContrastEnhance(rgb):
+    # Convert RGB to LAB
+    lab = cv2.cvtColor(rgb, cv2.COLOR_RGB2LAB)
+
+    # Get L channel
+    l = lab[:, :, 0]
+
+    # Calculate M value
+    M = np.mean(l)
+    if M < 128:
+        M = 127 - (127 - M) / 2
+    else:
+        M = 128 + M / 2
+
+    # Precompute the fuzzy transform
+    x = list(range(-50, 306))
+    FuzzyTransform = dict(zip(x, [Infer(np.array([i]), M) for i in x]))
+
+    # Apply the transform to l channel
+    u, inv = np.unique(l, return_inverse=True)
+    l = np.array([FuzzyTransform[i] for i in u])[inv].reshape(l.shape)
+
+    # Min-max scale the output L channel to fit (0, 255):
+    Min = np.min(l)
+    Max = np.max(l)
+    lab[:, :, 0] = (l - Min) / (Max - Min) * 255
+
+    # Convert LAB to RGB
+    return cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+
+
+# Traditional method of histogram equalization
+def HE(rgb):
+    lab = cv2.cvtColor(rgb, cv2.COLOR_RGB2LAB)
+    lab[:, :, 0] = cv2.equalizeHist(lab[:, :, 0])
+    return cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+
+
+# Contrast Limited Adaptive Histogram Equalization
+def CLAHE(rgb):
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    lab = cv2.cvtColor(rgb, cv2.COLOR_RGB2LAB)
+    lab[:, :, 0] = clahe.apply(lab[:, :, 0])
+    return cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+
+
+data = np.array([cv2.cvtColor(cv2.imread(p), cv2.COLOR_BGR2RGB) for p in glob(f'{PATH}/*')])
+data.shape
+for i in range(data.shape[0]):
+    img = data[i]
+    fce = FuzzyContrastEnhance(img)
+    he = HE(img)
+    clahe = CLAHE(img)
+    display(Markdown(f'### <p style="text-align: center;">Sample Photo {i + 1}</p>'))
+    plt.figure(figsize=(15, 10))
+    plt.subplot(2, 2, 1)
+    plt.imshow(data[i])
+    plt.title('Original Image')
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(fce)
+    plt.title('Fuzzy Contrast Enhance')
+
+    plt.subplot(2, 2, 3)
+    plt.imshow(he)
+    plt.title('Traditional HE')
+
+    plt.subplot(2, 2, 4)
+    plt.imshow(clahe)
+    plt.title('CLAHE')
+
+    plt.show()
